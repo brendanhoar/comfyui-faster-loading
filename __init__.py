399d2eb637c9902cfca0905359cfe187b719542b
Brendan Hoar <brendan.hoar@gmail.com>
2026-03-06 17:55:59 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # TODO
 # 1. Keep track of recent full reads of each filename, don't reread if less than x minutes since last request. I am seeing a lot more back-to-back calls to these with the same model path in recent comfyui releases.
 # 2. Chunk reads into 64MB chunks to reduce system freezes during a system call.
-# 3. Use mmap to read instead of read().
+# 3. Use mmap to read instead of read(). Is there a way to populate the page cache from the storage without the copy of the data to process memory?
 # 4. Add PrefetchVirtualMemory before read, as it might maybe make it fast, or maybe not.
 
 import time

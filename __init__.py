6bd747d7bb803e3fe4cf2e1e6b8e1b7f88a28e6d
Brendan Hoar <brendan.hoar@gmail.com>
2026-03-06 20:42:07 -0500

    Update comments in __init__.py
    Add a new comment regarding PrefetchVirtualMemory and reading strategy.

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@
 # 4. Add PrefetchVirtualMemory before read, as it might maybe make it fast, or maybe not.
 # 5. If #1 does not suffice, add a heuristic that can tell if the file is mostly loaded into page cache or not (perhaps generate a random start page number, then read 4MB chunks spaced at 32MB distances, and check the timing?)
 # 6. Based on experiments, simply creating a memory map and marking the region as PrefetchVirtualMemory without reading the file does *not* make actual page faults (such as from CUDA DMA copies to the GPU) any faster. It has to be process reads of the page to get the OS to do the readahead.
+# 7. PrefetchVirtualMemory and read first byte, then every +4k byte until the end of file? Maybe less memcpy than reading/discarding 4MB at a time? Which is faster?
 
 import time
 import torch
